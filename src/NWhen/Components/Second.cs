c7237ad058378fd65c5730c3be8450b6b80cf71f
Radu Ungureanu <radu.ungureanu@outlook.com>
2024-07-01 13:25:21 +0300

    Removed TODO after reading the documentation at https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.5: "The BYSECOND rule part specifies a COMMA-separated list of seconds within a minute.  Valid values are 0 to 60."

--- src/NWhen/Components/Second.cs
+++ src/NWhen/Components/Second.cs
@@ -7,7 +7,6 @@ public class Second(int second) : BetweenComponent<InvalidSecondException>(secon
 {
     protected override int MinValue => 0;
 
-    // TODO: shouldn't this be between 0 and 59?
     protected override int MaxValue => 60;
 
     public static implicit operator Second(int second) => new(second);
